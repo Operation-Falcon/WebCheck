ba0e70842b2a3cd269e6a38872300f45f15c9b83
Operation Falcon <83413793+operationfalcon@users.noreply.github.com>
2021-05-04 19:53:49 +0530

    Update function.py

--- function/function.py
+++ function/function.py
@@ -13,21 +13,3 @@ def web_check(domain):
     except Exception as e:
         pass
 
-def web_check_file(filename):
-    try:
-        with open(filename, 'r') as file:
-            file=file.readlines()
-            for i in range(0, len(file)):
-                r=requests.get(url=str("http://"+file[i]), allow_redirects=False, timeout=20)
-                try:
-                    if r.status_code==200:
-                        print("http://"+file[i])
-                    elif r.status_code !=200:
-                        r = requests.get(url="https://"+file[i], timeout=20)
-                        if r.status_code == 200:
-                            print("https://"+file[i])
-                except Exception as e:
-                    print("Error caused")
-
-    except Exception as e:
-        print("Max retries exceeded with url. Caused by NewConnectionError")
